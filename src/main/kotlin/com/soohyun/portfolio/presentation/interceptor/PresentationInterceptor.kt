525b97c172da37f8bb339e06487b3b5531440331
Soohyun Lee <sb05037@gmail.com>
2024-10-16 11:57:35 +0900

    인터셉터 개발

--- src/main/kotlin/com/soohyun/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/soohyun/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,23 @@
 package com.soohyun.portfolio.presentation.interceptor
 
+import com.soohyun.portfolio.domain.entity.HttpInterface
+import com.soohyun.portfolio.domain.repository.HttpInterfacetRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+        private val httpInterfacetRepository: HttpInterfacetRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfacetRepository.save(httpInterface)
+    }
+}
