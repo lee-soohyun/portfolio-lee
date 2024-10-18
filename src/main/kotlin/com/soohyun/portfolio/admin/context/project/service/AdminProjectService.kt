b554bedaa2a8cf749dcf5545402d17738d7c3030
Soohyun Lee <sb05037@gmail.com>
2024-10-19 01:20:14 +0900

    삽입, 수정, 조회 API 개발 - 연관관계 일대다

--- src/main/kotlin/com/soohyun/portfolio/admin/context/project/service/AdminProjectService.kt
+++ src/main/kotlin/com/soohyun/portfolio/admin/context/project/service/AdminProjectService.kt
@@ -1,11 +1,13 @@
 package com.soohyun.portfolio.admin.context.project.service
 
+import com.soohyun.portfolio.admin.context.project.form.ProjectForm
 import com.soohyun.portfolio.admin.data.TableDTO
 import com.soohyun.portfolio.admin.exception.AdminBadRequestException
 import com.soohyun.portfolio.domain.entity.Project
 import com.soohyun.portfolio.domain.entity.ProjectDetail
 import com.soohyun.portfolio.domain.repository.ProjectRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AdminProjectService(
@@ -26,5 +28,46 @@ class AdminProjectService(
             .details else emptyList()
 
         return TableDTO.from(classInfo, entities)
+    }
+    @Transactional
+    fun save(form: ProjectForm) {
+        val expericeDetails = form.details
+            ?.map { detail -> detail.toEntity() }
+            ?.toMutableList()
+
+        val project = form.toEntity()
+        project.addDetails(expericeDetails)
+
+        projectRepository.save(project)
+    }
+
+    @Transactional
+    fun update(id: Long, form: ProjectForm) {
+        val project = projectRepository.findById(id)
+            .orElseThrow { throw  AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수가 없습니다.") }
+
+        project.update(
+            name = form.name,
+            description = form.description,
+            startYear = form.startYear,
+            startMonth = form.startMonth,
+            endYear = form.endYear,
+            endMonth = form.endMonth,
+            isActive = form.isActive
+        )
+
+        val detailMap = project.details.map { it.id to it }.toMap()
+        form.details?.forEach{
+            val entity = detailMap.get(it.id)
+            if(entity != null) {
+                entity.update(
+                    content = it.content,
+                    url = it.url,
+                    isActive = it.isActive
+                )
+            } else{
+                project.details.add(it.toEntity())
+            }
+        }
     }
 }
